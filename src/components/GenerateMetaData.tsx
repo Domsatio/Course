3d30c4485fedaf6152ee073f0d38b9a9c8f7dde1
GhozyIkbarF <ghozyikbarf@gmail.com>
2024-09-25 00:13:27 +0700

    add image metadata

--- src/components/GenerateMetaData.tsx
+++ src/components/GenerateMetaData.tsx
@@ -3,15 +3,18 @@ import Head from "next/head";
 export default function GenerateMetaData({
   title = "",
   desc = "",
+  thumbnail = "",
 }: {
   title: string;
   desc?: string;
+  thumbnail?: string;
 }) {
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={desc} />
       <meta property="og:title" content={title} />
+      {thumbnail && <meta property="og:image" content={thumbnail} />}
       <meta property="og:description" content={desc} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
