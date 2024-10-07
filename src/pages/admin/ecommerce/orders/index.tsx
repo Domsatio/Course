eb68bbc14703c746583484944ac5d1cc92eeed8e
Yanda Agil <yandaagil@gmail.com>
2024-10-07 21:22:09 +0700

    refactor: Add DetailOrderAdmin component for viewing order details

--- src/pages/admin/ecommerce/orders/index.tsx
+++ src/pages/admin/ecommerce/orders/index.tsx
@@ -1,142 +1,124 @@
-// import React, { useState } from "react";
-// import {
-//   Typography,
-//   Avatar,
-//   Chip,
-// } from "@material-tailwind/react";
-// import { GetOrder } from "@/types/order.type";
-// import TableData, { TableActionProps } from "@/components/admin/TableData";
-// import { orderServices } from "@/services/serviceGenerator";
-// import { dateFormater } from "@/utils/date";
-// import { NullProof } from "@/helpers/appFunction";
+import React, { useState } from "react";
+import {
+  Typography,
+  Avatar,
+  Chip,
+} from "@material-tailwind/react";
+import { Order } from "@/types/order.type";
+import TableData, { TableActionProps } from "@/components/admin/TableData";
+import { orderServices } from "@/services/serviceGenerator";
+import { dateFormater } from "@/utils/date";
+import { ConvertCurrency, NullProof } from "@/helpers/appFunction";
 
-// type DataProps = {
-//   data: GetOrder[];
-//   page: number;
-//   size: number;
-// }
+type DataProps = {
+  data: Order[];
+  page: number;
+  size: number;
+}
 
-// const TABLE_HEAD = [
-//   "Date",
-//   "Product",
-//   "Quantity",
-//   "Price",
-//   "Ordered by",
-//   "Status",
-//   "Action"
-// ];
+const TABLE_HEAD = [
+  "Date",
+  "Total Items",
+  "Price",
+  "Customer",
+  "Status",
+  "Action"
+];
 
-// export default function Index() {
-//   const [data, setData] = useState<DataProps>({
-//     data: [],
-//     page: 0,
-//     size: 0,
-//   });
-//   const { Table, TableAction } = TableData({
-//     title: "Orders",
-//     description: "List of orders",
-//     tableHeader: TABLE_HEAD,
-//     realtimeTable: "Order",
-//     onSuccess: (data: DataProps) => setData(data),
-//     service: orderServices,
-//   });
+export default function Index() {
+  const [data, setData] = useState<DataProps>({
+    data: [],
+    page: 0,
+    size: 0,
+  });
+  const { Table, TableAction } = TableData({
+    title: "Orders",
+    description: "List of orders",
+    tableHeader: TABLE_HEAD,
+    realtimeTable: "Order",
+    onSuccess: (data: DataProps) => setData(data),
+    service: orderServices,
+    isActionAdd: false
+  });
 
-//   const orderStatus = (status: string) => {
-//     if (status === "PENDING") {
-//       return "pink"
-//     } else if (status === "PROCESSING") {
-//       return "amber"
-//     } else if (status === "SHIPPING") {
-//       return "blue"
-//     } else if (status === "DELIVERED") {
-//       return "green"
-//     } else if (status === "CANCELLED") {
-//       return "red"
-//     }
-//   }
+  return Table(
+    NullProof({
+      input: data,
+      params: "data",
+      isMap: true,
+    }).map((order: Order, index: number) => {
+      const isLast = index === data.data.length - 1;
+      const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+      const totalItems = order.products.reduce((acc, curr) => acc + curr.quantity, 0);
+      return (
+        <tr key={order.id}>
+          <td className={classes}>
+            <Typography
+              variant="small"
+              color="blue-gray"
+              className="font-normal"
+            >
+              {dateFormater(String(order.transactionTime), "short")}
+            </Typography>
+          </td>
+          <td className={classes}>
+            <Typography
+              variant="small"
+              color="blue-gray"
+              className="font-normal"
+            >
+              {totalItems}
+            </Typography>
+          </td>
+          <td className={classes}>
+            <Typography
+              variant="small"
+              color="blue-gray"
+              className="font-normal"
+            >
+              {ConvertCurrency(order.grossAmount)}
+            </Typography>
+          </td>
+          <td className={classes}>
+            <Typography
+              variant="small"
+              color="blue-gray"
+              className="font-normal"
+            >
+              {order.user.name}
+            </Typography>
+          </td>
+          <td className={classes}>
+            <Typography
+              variant="small"
+              color="blue-gray"
+              className="font-normal flex"
+            >
+              <Chip
+                variant="ghost"
+                color={order.transactionStatus === "settlement" || order.transactionStatus === "capture" ? "green" : "yellow"}
+                size="sm"
+                value={order.transactionStatus === "settlement" || order.transactionStatus === "capture" ? "Completed" : "Waiting Payment"}
+                icon={
+                  <span
+                    className={`mx-auto mt-1 block h-2 w-2 rounded-full content-[''] ${order.transactionStatus === "settlement" || order.transactionStatus === "capture" ? "bg-green-900" : "bg-yellow-900"
+                      }`}
+                  />
+                }
+              />
+            </Typography>
+          </td>
+          <td className={classes}>
+            <TableAction data={dataAction} id={order.id} />
+          </td>
+        </tr>
+      );
+    })
+  );
+}
 
-//   return Table(
-//     NullProof({
-//       input: data,
-//       params: "data",
-//       isMap: true,
-//     }).map((order: GetOrder, index: number) => {
-//       const isLast = index === data.data.length - 1;
-//       const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
-//       return (
-//         <tr key={order.id}>
-//           <td className={classes}>
-//             <Typography
-//               variant="small"
-//               color="blue-gray"
-//               className="font-normal"
-//             >
-//               {NullProof({ input: order, params: "date" })}
-//             </Typography>
-//           </td>
-//           <td className={classes}>
-//             <Typography
-//               variant="small"
-//               color="blue-gray"
-//               className="font-normal"
-//             >
-//               {NullProof({ input: order, params: "product" }).name}
-//             </Typography>
-//           </td>
-//           <td className={classes}>
-//             <Typography
-//               variant="small"
-//               color="blue-gray"
-//               className="font-normal"
-//             >
-//               {NullProof({ input: order, params: "quantity" })}
-//             </Typography>
-//           </td>
-//           <td className={classes}>
-//             <Typography
-//               variant="small"
-//               color="blue-gray"
-//               className="font-normal"
-//             >
-//               {NullProof({ input: order, params: "price" })}
-//             </Typography>
-//           </td>
-//           <td className={classes}>
-//             <div className="flex items-center">
-//               <Avatar
-//                 src={NullProof({ input: order, params: "user" }).avatar}
-//               />
-//               <Typography
-//                 variant="small"
-//                 color="blue-gray"
-//                 className="font-normal ml-2"
-//               >
-//                 {NullProof({ input: order, params: "user" }).name}
-//               </Typography>
-//             </div>
-//           </td>
-//           <td className={classes}>
-//             <Chip
-//               value={order.status}
-//             />
-//           </td>
-//           <td className={classes}>
-//             {TableAction({ data: dataAction, id: order.id })}
-//           </td>
-//         </tr>
-//       );
-//     })
-//   );
-// }
-
-// const dataAction: TableActionProps[] = [
-//   {
-//     action: "view",
-//   },
-//   {
-//     action: "update",
-//   },
-//   {
-//     action: "delete",
-//   },
-// ];
+const dataAction: TableActionProps[] = [
+  {
+    action: "view",
+  }
+];
